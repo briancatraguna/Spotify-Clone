a49df051176e9915454c53481cc845c55729c85a
briancatraguna <briancatraguna@rocketmail.com>
2021-08-18 01:11:53 +0700

    Refactored redirect URL for development

--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -4,7 +4,7 @@ import './style.css';
 
 
 const LoginButton = () => {
-    const MY_WEBSITE: string = "https://spotifynya-brian.vercel.app/callback"
+    const MY_WEBSITE: string = "http://localhost:3000/callback"
     const REDIRECT_URL: string = `https://accounts.spotify.com/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=token&redirect_uri=${MY_WEBSITE}&scope=playlist-modify-private`
 
     return (
