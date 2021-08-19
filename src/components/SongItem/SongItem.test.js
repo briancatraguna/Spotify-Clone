93e3bf687a24c330a639fdc8895d0ccb38a14e61
briancatraguna <briancatraguna@rocketmail.com>
2021-08-19 20:08:45 +0700

    Added userEvent in test

--- src/components/SongItem/SongItem.test.js
+++ src/components/SongItem/SongItem.test.js
@@ -1,19 +1,24 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render,screen } from '@testing-library/react';
-import SongItem from './components/SongItem/index';
-import data from './utils/albumData'
+import userEvent from '@testing-library/user-event';
+import SongItem from './index';
+import data from '../../utils/albumData'
 
 for (let i = 0;i < data.length; i++){
   it(`Item ${i}: ${data[i].album.name}`,()=>{
   
     render(
       <SongItem
+          type="normal"
+          pushToSelectedList={()=>{}}
+          deleteFromSelectedList={()=>{}}
           status={false}
           id={"1"}
           imgUrl={data[i].album.images[0].url}
           songTitle={data[i].album.name}
           artist={data[i].artists[0].name}
+          artistLink={data[i].artists[0].external_urls.spotify}
           />
     )    
     
@@ -26,5 +31,10 @@ for (let i = 0;i < data.length; i++){
     expect(screen.getByText(data[i].album.name)).toBeInTheDocument();
     //artist
     expect(screen.getByText(data[i].artists[0].name));
+
+    //Click select
+    const selectButton = screen.getByText("Select");
+    userEvent.click(selectButton)
+    expect(screen.getByText("Deselect")).toBeInTheDocument();
   });
 }
